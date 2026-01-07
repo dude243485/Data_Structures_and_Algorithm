3aa22dd526bb9671ed1b498e5e17b7b92bc975f6
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-01-07 11:55:43 +0100

    Day 7(longest substring without repeating characters)

---
+++ leetcode/longest substring without repeating characters/solution1.py
@@ -0,0 +1,20 @@
+"""sliding window solution"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            
+            res = max(res, r - l + 1)
+        return res
+        
+                
+input_data = "abcabcbb"      
+test = Solution()
+print(test.lengthOfLongestSubstring(input_data))
