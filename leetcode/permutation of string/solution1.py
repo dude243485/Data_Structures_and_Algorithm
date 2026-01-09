7e6d9380a63d737ddd8cbe19d0550191f5032232
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-01-09 17:00:00 +0100

    Day 9(Permutation in String)

---
+++ leetcode/permutation of string/solution1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        length = len(s1)
+        left = 0
+        s1 = "".join(sorted(s1))
+        print(s1)
+        for right in range(left+length-1, len(s2)):
+            current = "".join(sorted(s2[left:right+1]))
+            print(current)
+            if (current == s1):
+                return True
+            left += 1
+        return False
+            
+     
+s1 = "ab"
+s2 = "eidbaooo"     
+test = Solution()
+print(test.checkInclusion(s1, s2))
