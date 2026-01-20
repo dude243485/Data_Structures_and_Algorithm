0c2a6482250facf56d72b4c24256f255c6d3bc3a
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-01-20 09:57:40 +0100

    Day 10(some easys)

---
+++ leetcode/1929 - Concatenation of arrays/solution1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+        
+        
+        
+
+nums = [1,2,1]
+test = Solution()
+print(test.getConcatenation(nums))
