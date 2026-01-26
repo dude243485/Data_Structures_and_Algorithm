173edf3a9ff90fa9c918a888c95522217d33eac5
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-01-26 09:39:01 +0100

    My solution worked better than the neetcode provided solution

--- leetcode/36 - Valid Sudoku/solution1.py
+++ leetcode/36 - Valid Sudoku/solution1.py
@@ -1,31 +1,48 @@
 from typing import List
-
 class Solution:
     def isValidSudoku(self, board: List[list[str]]) -> bool:
         map = {}
-        for i in range(9):
-            map[i+"r"] = []
-            map[i +"c"] = []
+        for i in range(1, 10):
+            map[str(i)] = []
         
-        print(map)
+        
         
         rows, cols = len(board), len(board[0])
-        
         for r in range(rows):
-            
             for c in range(cols):
-                if board[r][c].isalpha() and (r in map[r+"r"] or c in map[c+"c"]):
-                    return False
-                elif not board[r][c].isalpha():
-                    continue
-                
-                else:
-                    map[i+"r"].append(r)
-                    map[i +"c"].append(c)
+                if board[r][c] != "." and map[board[r][c]] == []:
+                    print("first inputs")
+                    map[board[r][c]].append([r, c])
+                elif board[r][c] != "." and len(map[board[r][c]]) > 0:
+                    #check rows and columns
+                    for i in map[board[r][c]]:
+                        if r == i[0] or c == i[1]:
+                            
+                            return False
+                        
+                    #check for 3x3 boxes
+                    for i in map[board[r][c]]:
+                        if r // 3 == i[0] // 3 and c // 3 == i[1] // 3:
+                            return False   
+                    map[board[r][c]].append([r, c])
+                   
         return True
                     
-
-                    
-                
                 
         
+          
+       
+    
+
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+test = Solution()
+answer = test.isValidSudoku(board)
+print(answer)
