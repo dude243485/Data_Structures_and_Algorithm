f20c4d86c3568c2eb9e7b3946acb816d36f8355e
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-02-07 15:54:40 +0100

    forgot to subtract 1

--- leetcode/167 - Two sum ii - Input array is sorted/solution1.py
+++ leetcode/167 - Two sum ii - Input array is sorted/solution1.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0 , len(numbers)
+        l, r = 0 , len(numbers) - 1
         while l < r:
             sum = numbers[l] + numbers[r]
             if sum < target:
